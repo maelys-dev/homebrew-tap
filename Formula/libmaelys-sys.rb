799db5781b81fdd13b28ab6ef969c366b72ead5e
davidb987654 <davidb987654@users.noreply.github.com>
2026-09-05 07:11:23 +0000

    Update libmaelys-sys to 0.8.1

--- Formula/libmaelys-sys.rb
+++ Formula/libmaelys-sys.rb
@@ -5,22 +5,22 @@
 class LibmaelysSys < Formula
   desc "Minimal callback-free POSIX systems foundation for C"
   homepage "https://github.com/maelys-dev/maelys-system"
-  url "https://github.com/maelys-dev/maelys-system/archive/refs/tags/v0.8.0.tar.gz"
-  sha256 "b1461e567659d109b40ae41fabfcce561f5a89ed2c10b2685565c798120a07a5"
+  url "https://github.com/maelys-dev/maelys-system/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "dcffe2458b846333640f54a9b31c0afeee1dbd01849914b8c6bb88044d3c88a3"
   license "MPL-2.0"
 
   bottle do
-    root_url "https://github.com/maelys-dev/maelys-system/releases/download/v0.8.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a226b638552438f9211f6e69cbc18e75a86995fe3ccfe4e370eb68ec6fcd08af"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bccf0fce517a9d4ce82b8f5a8b510d618acd58cc93da7fee452676862a5cf154"
+    root_url "https://github.com/maelys-dev/maelys-system/releases/download/v0.8.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "37679300d3e4232579c952271e76456c37cfc4dfe2c2aeb167018423ad4c4f0b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c40fe5da186d6d5ac60f385ed4c894110e110187d9f23b32fd6a9bd42924ec59"
   end
 
   # maelys-warden still vendors this library and its headers.
   conflicts_with "maelys-warden", because: "both install libmaelys_sys and the Maelys System headers"
 
   def install
-    system "make", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "check"
-    system "make", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "PREFIX=#{prefix}", "install"
+    system "make", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "WERROR=", "check"
+    system "make", "CC=#{ENV.cc}", "CXX=#{ENV.cxx}", "WERROR=", "PREFIX=#{prefix}", "install"
   end
 
   test do
